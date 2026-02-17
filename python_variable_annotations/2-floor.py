422d4131c69afc0a12e013ddee35a982d1ac28f6
rateelbahathiq <rateelbahathiq@gmail.com>
2026-02-17 08:46:38 +0000

    add task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that provides a function to return the floor of a float."""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float."""
+    return math.floor(n)
