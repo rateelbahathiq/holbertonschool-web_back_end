df7d4010c9a8c6fe8ba09ff6ed4808be3f316e74
rateelbahathiq <rateelbahathiq@gmail.com>
2026-02-17 08:17:35 +0000

    fix spacing

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,5 +2,5 @@
 """Module that provides a function to concat two strings."""
 
 def concat(str1: str, str2: str) -> str:
-    """Return a concatenated string. """
+    """Return a concatenated string."""
     return str1 + str2 
