7a27f8b6fed70859b21672021fe5a4b55d589819
rateelbahathiq <rateelbahathiq@gmail.com>
2026-02-17 08:15:49 +0000

    add task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module that provides a function to concat two strings."""
+
+def concat(str1: str, str2: str) -> str:
+    """Return a concatenated string. """
+    return str1 + str2 
