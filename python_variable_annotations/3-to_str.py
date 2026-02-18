edf5578ca3b5428f8d890d435937d9d401451a58
rateelbahathiq <rateelbahathiq@gmail.com>
2026-02-18 07:59:46 +0000

    fix error

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Module that provides a function to return the string representation of a float."""
+"""Module that provides a function to return the string
+representation of a float."""
 
 
 def to_str(n: float) -> str:
