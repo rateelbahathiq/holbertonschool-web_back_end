94761465699551c870031cc8f857121bbad3a122
rateelbahathiq <rateelbahathiq@gmail.com>
2026-02-18 09:11:19 +0000

    fix error

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -7,9 +7,7 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return a function that multiplies a float by multiplier."""
-    
     def multiply(x: float) -> float:
         """Multiply x by the captured multiplier."""
         return x * multiplier
-
     return multiply
