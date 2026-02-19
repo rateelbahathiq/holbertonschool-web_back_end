24a3c035194f51ee182973df820df8bbc6889405
rateelbahathiq <rateelbahathiq@gmail.com>
2026-02-19 08:51:29 +0000

    fix error

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """
-Async generator
+Module for async generator
 """
-
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
-    Yields 10 random numbers between 0 and 10,
-    waiting 1 second between each yield.
+    Async generator that yields random numbers
+
+    Yields:
+        Random float between 0 and 10, after waiting 1 second each time
     """
     for _ in range(10):
         await asyncio.sleep(1)
