87d250832c5d229b111b764e57d3bbbadfc55d2c
rateelbahathiq <rateelbahathiq@gmail.com>
2026-02-19 08:40:52 +0000

    add task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Async generator example
+"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Asynchronously generates 10 random numbers
+    between 0 and 10, waiting 1 second between each.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
