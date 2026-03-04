a771a1db8c5ae4a685dce7aabde59210c90d7358
rateelbahathiq <rateelbahathiq@gmail.com>
2026-03-04 09:50:20 +0000

    update task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-"""Simple pagination."""
+"""
+Simple pagination
+"""
 import csv
 import math
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return a tuple of start and end index for pagination."""
-    start = (page -1) * page_size
+    """Return a tuple of (start_index, end_index) for pagination."""
+    start = (page - 1) * page_size
     end = start + page_size
     return start, end
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -31,14 +34,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """Return the correct page of the dataset."""
-            assert isinstance(page, int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
+        """Return the correct page of the dataset."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-            start, end = index_range(page, page_size)
-            data = self.dataset()
+        start, end = index_range(page, page_size)
+        data = self.dataset()
 
-            if start >= len(data):
-                return []
+        if start >= len(data):
+            return []
 
-            return data[start: end]
+        return data[start:end]
