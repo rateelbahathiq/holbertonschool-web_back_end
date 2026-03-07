fede79badec4ab873b7dd06d3c1f4aa09fd9d326
rateelbahathiq <rateelbahathiq@gmail.com>
2026-03-07 21:29:41 +0300

    fix error

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-"""Provides stats about Nginx logs stored in MongoDB"""
-
+"""12-log_stats.py"""
 from pymongo import MongoClient
-
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
@@ -10,14 +8,11 @@ if __name__ == "__main__":
 
     total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
-    print("Methods:")
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
+    print("Methods:")
+    for method in ("GET", "POST", "PUT", "PATCH", "DELETE"):
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
