f16881bb6df8548b1e191225c77f167612de7460
rateelbahathiq <rateelbahathiq@gmail.com>
2026-03-09 07:45:32 +0000

    add task 10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
+
+  for (const value of array) {
+    array[i] = appendString + value;
+    i += 1;
+  }
+
+  return array;
+}
