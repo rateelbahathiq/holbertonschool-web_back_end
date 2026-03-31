2b4fe3f3a8bf27ee2a2e6a1fbfdb83e70199aa5c
rateelbahathiq <rateelbahathiq@gmail.com>
2026-03-31 08:41:11 +0000

    fix error

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,17 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value,
+      };
+    }
+
+    return {
+      status: result.status,
+      value: String(result.reason),
+    };
+  }));
 }
