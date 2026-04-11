efc25b65c248d2b8ecd33107cf6d93efa5bbbad6
rateelbahathiq <rateelbahathiq@gmail.com>
2026-04-11 13:37:35 +0000

    fix error

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.trim()}\n`);
+  process.stdout.write(`Your name is: ${data.toString()}`);
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
-process.stdin.resume();
